09655c712cbeb6f5df6dea70c76e355f87b1dcb2
Imran107 <imran.itim@gmail.com>
2021-12-29 18:43:39 +0500

    Implement Internationalization

--- src/main/java/com/in28minutes/rest/webservices/springbootrestservices/helloworld/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/springbootrestservices/helloworld/HelloWorldController.java
@@ -1,12 +1,20 @@
 package com.in28minutes.rest.webservices.springbootrestservices.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+	
+	@Autowired
+	MessageSource messageResource;
 	
 	@RequestMapping(method = RequestMethod.GET, path="/hello-world")
 	public String helloWorld(){
@@ -21,6 +29,14 @@ public class HelloWorldController {
 	@RequestMapping(method = RequestMethod.GET, path="/hello-world/path-variable/{name}")
 	public String helloWorldBeanPathVariable(@PathVariable String name){
 		return String.format("Hello %s", name); 
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, path="/good-morning-internationalized")
+	public String goodMorningInternationalized(
+			@RequestHeader(name="Accept-Language", required=false) Locale locale
+			){
+		return messageResource.getMessage("good.morning.message", 
+				null, "Default Message", locale);
 	}
 
 }
