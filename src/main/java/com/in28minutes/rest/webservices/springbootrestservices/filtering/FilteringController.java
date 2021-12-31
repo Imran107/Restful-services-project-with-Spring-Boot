66b5e1ef86c78f483af227cc4f703a312603295f
Imran107 <imran.itim@gmail.com>
2021-12-31 20:41:32 +0500

    Filtering specific fields to not expose in webservice response

---
+++ src/main/java/com/in28minutes/rest/webservices/springbootrestservices/filtering/FilteringController.java
@@ -0,0 +1,34 @@
+package com.in28minutes.rest.webservices.springbootrestservices.filtering;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.converter.json.MappingJacksonValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+
+@RestController
+public class FilteringController {
+
+	// field1,field2
+	@GetMapping("/filtering")
+	public SomeBean retrieveSomeBean() {
+		SomeBean someBean = new SomeBean("value1", "value2", "value3");
+
+		return someBean;
+	}
+
+	// field2, field3
+	@GetMapping("/filtering-list")
+	public List<SomeBean> retrieveListOfSomeBeans() {
+		List<SomeBean> list = Arrays.asList(new SomeBean("value1", "value2", "value3"),
+				new SomeBean("value12", "value22", "value32"));
+
+		return list;
+	}
+
+}
